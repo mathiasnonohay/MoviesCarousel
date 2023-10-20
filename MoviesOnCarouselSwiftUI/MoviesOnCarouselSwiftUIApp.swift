4ad02d87ee1b2df0fb6ac6694e6c4ca8bef63da9
Mathias Nonohay <mathias.gianotti.nonohay@sibs.com>
2023-10-20 13:03:41 +0100

    Initial Commit

---
+++ MoviesOnCarouselSwiftUI/MoviesOnCarouselSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesOnCarouselSwiftUIApp.swift
+//  MoviesOnCarouselSwiftUI
+//
+//  Created by Mathias Nonohay on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesOnCarouselSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
